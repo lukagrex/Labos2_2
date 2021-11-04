dcb08f0fdea53f3f36d5c103fc597f596973bbdb
Luka Gregorić <endo145@gmail.com>
2021-11-04 18:17:59 +0100

    Add project files.

---
+++ Labos2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labos2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticEquation qe = new QuadraticEquation(1, 2, -3);
+            //Console.WriteLine(qe.A);
+            //qe.A = 5;
+            var roots1 = qe.Roots;
+            Console.WriteLine(roots1[0]);
+            Console.WriteLine(roots1[1]);
+
+            QuadraticEquation qe1 = new QuadraticEquation(-1, 2, -1);
+            var roots2 = qe1.Roots;
+            Console.WriteLine(roots2[0]);
+            Console.WriteLine(roots2[1]);
+            Console.ReadKey(false);
+        }
+    }
+}
---
+++ Labos2_2/QuadraticEquation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labos2_2
+{
+    public class QuadraticEquation
+    {
+
+        public QuadraticEquation()
+        {
+            this.a = 0;
+            this.b = 0;
+            this.c = 0;
+        }
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public double Y(double x)
+        {
+            return a * x * x + b * x + c;
+        }
+
+        public double Discriminant
+        {
+            get { return b * b - 4 * a * c; }
+        }
+
+        public double[] Roots
+        {
+            get
+            {
+                if (a == 0)
+                {
+                    return new double[] { -c / b, -c / b };
+                }
+                double sqrtDisc = Math.Sqrt(Discriminant);
+                return new double[] { ((-b - sqrtDisc) / (2 * a)), ((-b + sqrtDisc) / (2 * a)) };
+            }
+        }
+
+        private double a;
+        private double b;
+        private double c;
+    }
+}
